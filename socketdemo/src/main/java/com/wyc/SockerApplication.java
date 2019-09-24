81e159b977a205ae263c26cb3302760413cddea6
wangyuanchen <674392181@qq.com>
2019-09-24 15:00:24 +0800

    首次上传

---
+++ socketdemo/src/main/java/com/wyc/SockerApplication.java
@@ -0,0 +1,12 @@
+package com.wyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockerApplication.class,args);
+    }
+}
